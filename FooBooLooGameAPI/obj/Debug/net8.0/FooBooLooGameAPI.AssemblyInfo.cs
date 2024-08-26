ca3d77746a8632cbc7e3135962da11b646f8c0fa
Andy Tran <rooneytai5236@gmail.com>
2024-08-27 03:48:18 +1000

    added design diagrams

--- FooBooLooGameAPI/obj/Debug/net8.0/FooBooLooGameAPI.AssemblyInfo.cs
+++ FooBooLooGameAPI/obj/Debug/net8.0/FooBooLooGameAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FooBooLooGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4036a758e0531cf00a067477d5d16009f2cb560e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814adef4d5cbf20b430e0f02209ff094e90dac16")]
 [assembly: System.Reflection.AssemblyProductAttribute("FooBooLooGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FooBooLooGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
